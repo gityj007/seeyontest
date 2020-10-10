aac315b54552b1e71dbfd1cf5aad100b5b68e04b
gityj007 <miryangjian@aliyun.com>
2020-10-10 15:25:07 +0800

    第一次提交

---
+++ src/main/java/com/seeyon/orgcenter/OrgcenterApplication.java
@@ -0,0 +1,13 @@
+package com.seeyon.orgcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seeyon/orgcenter/OrgcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeyon.orgcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
