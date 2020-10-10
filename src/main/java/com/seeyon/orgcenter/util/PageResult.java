453681df77235d31a89471e860eed8bb80a59c55
gouyu <gouyudexiaoxin@outlook.com>
2020-10-10 17:11:19 +0800

    提交一点东西

---
+++ src/main/java/com/seeyon/orgcenter/util/PageResult.java
@@ -0,0 +1,81 @@
+package com.seeyon.orgcenter.util;
+
+
+
+/**
+ * @Author 勾瑜
+ * @Description: Api接口返回通用信息
+ * @Date 2020-10-10
+ */
+public class PageResult<T> extends BaseDomain {
+
+    /**
+     * 是否成功
+     */
+    private boolean flag;
+
+    private Integer code;
+
+    /**
+     * 如果失败，报错信息
+     */
+    private String message;
+
+    private T entity;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public T getEntity() {
+        return entity;
+    }
+
+    public void setEntity(T entity) {
+        this.entity = entity;
+    }
+
+    public PageResult() {
+        this.flag = true;
+    }
+
+    public void setError(String message){
+        this.flag = false;
+        this.message = message;
+    }
+
+    public void setSuccess(String message){
+        this.flag = true;
+        this.message = message;
+        this.code = 2000;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "flag=" + flag +
+                ", message='" + message + '\'' +
+                ", code=" + code +
+                '}';
+    }
+}
--- src/main/java/com/seeyon/orgcenter/util/testUtils.java
+++
@@ -1,3 +0,0 @@
-package com.seeyon.orgcenter.util;
-
-public class testUtils{}
