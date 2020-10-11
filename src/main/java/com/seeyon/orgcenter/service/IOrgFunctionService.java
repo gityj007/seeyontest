556ade1c16a3d85f1f791fb24469ecae9f917851
gouyu <gouyudexiaoxin@outlook.com>
2020-10-12 00:59:14 +0800

    添加功能service

---
+++ src/main/java/com/seeyon/orgcenter/service/IOrgFunctionService.java
@@ -0,0 +1,16 @@
+package com.seeyon.orgcenter.service;
+
+import com.seeyon.orgcenter.entity.OrgFunction;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yangjian
+ * @since 2020-10-12
+ */
+public interface IOrgFunctionService extends IService<OrgFunction> {
+
+}
