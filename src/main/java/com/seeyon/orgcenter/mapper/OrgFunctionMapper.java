7716f5ee878eec4ebbb6570c40bbd3b0f6e3a058
gouyu <gouyudexiaoxin@outlook.com>
2020-10-12 00:57:50 +0800

    添加功能mapper

---
+++ src/main/java/com/seeyon/orgcenter/mapper/OrgFunctionMapper.java
@@ -0,0 +1,16 @@
+package com.seeyon.orgcenter.mapper;
+
+import com.seeyon.orgcenter.entity.OrgFunction;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yangjian
+ * @since 2020-10-12
+ */
+public interface OrgFunctionMapper extends BaseMapper<OrgFunction> {
+
+}
