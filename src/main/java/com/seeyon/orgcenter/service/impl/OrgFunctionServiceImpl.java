19dd4bf2637e18d5590aae9e6a7c4c02abc9b9e0
gouyu <gouyudexiaoxin@outlook.com>
2020-10-12 00:59:41 +0800

    添加功能impl

---
+++ src/main/java/com/seeyon/orgcenter/service/impl/OrgFunctionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.seeyon.orgcenter.service.impl;
+
+import com.seeyon.orgcenter.entity.OrgFunction;
+import com.seeyon.orgcenter.mapper.OrgFunctionMapper;
+import com.seeyon.orgcenter.service.IOrgFunctionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author yangjian
+ * @since 2020-10-12
+ */
+@Service
+public class OrgFunctionServiceImpl extends ServiceImpl<OrgFunctionMapper, OrgFunction> implements IOrgFunctionService {
+
+}
--- src/test/java/com/seeyon/orgcenter/MpGenerator.java
+++ src/test/java/com/seeyon/orgcenter/MpGenerator.java
@@ -61,10 +61,10 @@ public class MpGenerator {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql:///seeyontest?useUnicode=true&characterEncoding=utf-8");
+        dsc.setUrl("jdbc:mysql:///seeyontest?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
-        dsc.setPassword("root123456");
+        dsc.setPassword("admin123456");
         mpg.setDataSource(dsc);
 
         // 包配置
@@ -137,12 +137,13 @@ public class MpGenerator {
         // 写于父类中的公共字段
 //        strategy.setSuperEntityColumns("id");
 
-        String[] tables = new String[5];
+        String[] tables = new String[6];
         tables[0] = "org_account";
         tables[1] = "org_app_account_role_relation";
         tables[2] = "org_permission_data_relation";
         tables[3] = "org_permission_fun_relation";
         tables[4] = "org_role";
+        tables[5] = "org_function";
 
         strategy.setInclude(tables);
         strategy.setControllerMappingHyphenStyle(true);
