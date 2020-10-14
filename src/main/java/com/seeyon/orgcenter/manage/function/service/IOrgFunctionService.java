63f999f6ea026e55060af9992509396990a7d136
gouyu <gouyudexiaoxin@outlook.com>
2020-10-14 11:14:06 +0800

    11

---
+++ src/main/java/com/seeyon/orgcenter/manage/function/service/IOrgFunctionService.java
@@ -0,0 +1,17 @@
+package com.seeyon.orgcenter.manage.function.service;
+
+import com.seeyon.orgcenter.common.baseservice.BaseService;
+import com.seeyon.orgcenter.manage.function.entity.OrgFunction;
+
+
+/**
+ * <p>
+ *  菜单服务的interface
+ * </p>
+ *
+ * @author gouyu
+ * @since 2020-10-12
+ */
+public interface IOrgFunctionService extends BaseService<OrgFunction>{
+
+}
---
+++ src/main/java/com/seeyon/orgcenter/manage/function/service/impl/OrgFunctionServiceImpl.java
@@ -0,0 +1,21 @@
+package com.seeyon.orgcenter.manage.function.service.impl;
+
+import com.seeyon.orgcenter.common.baseservice.impl.BaseServiceImpl;
+import com.seeyon.orgcenter.manage.function.entity.OrgFunction;
+import com.seeyon.orgcenter.manage.function.mapper.OrgFunctionMapper;
+import com.seeyon.orgcenter.manage.function.service.IOrgFunctionService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * <p>
+ *  菜单服务实现service类
+ * </p>
+ *
+ * @author gouyu
+ * @since 2020-10-12
+ */
+@Service
+public class OrgFunctionServiceImpl extends BaseServiceImpl<OrgFunctionMapper,OrgFunction> implements IOrgFunctionService{
+
+}
